f132c6240d65d731e2aa00e2216cb3128540ecad
Qia Wang <qiawang@ucsb.edu>
2025-08-28 21:22:12 -0700

    double quote

--- jobs/run.sh
+++ jobs/run.sh
@@ -7,5 +7,8 @@ CONTAINER_PATH="/ospool/ap40/data/qia.wang/container.sif"
 # Create project directory structure if needed
 mkdir -p output
 
+# Get the JSON parameters from command line argument
+PARAMS_JSON="$1"
+
 # Run the main simulation script using Singularity
-singularity exec --bind $(pwd):/work --pwd /work $CONTAINER_PATH julia --project=. run.jl ${PARAMS_JSON:+--params "$PARAMS_JSON"} --outdir output
+singularity exec --bind $(pwd):/work --pwd /work $CONTAINER_PATH julia --project=. run.jl --params "$PARAMS_JSON" --outdir output
